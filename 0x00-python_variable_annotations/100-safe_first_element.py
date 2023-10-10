18f84adafb7c62f1bd7d35816d8073c8119aadfb
Francis Ofori Anane <ofori.anane@gmail.com>
2023-10-10 23:42:37 +0000

    Modify file

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,8 @@
 """safe_first_element module"""
-from typing import Sequence, Any, Union
+from typing import Sequence, Any, Union, Optional
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Union[Any, None]]:
     """Returns the value at index 0 of the passed sequence or None for None types"""
     if lst:
         return lst[0]
