ee405dc91acc90c6182db7a25bded69e8c146a6e
Francis Ofori Anane <ofori.anane@gmail.com>
2023-10-10 21:18:04 +0000

    Add file

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,6 +3,7 @@
 """make_multiplier module"""
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a fuction that multiplies a float by 'multiplier' """
     return lambda a: a * multiplier
