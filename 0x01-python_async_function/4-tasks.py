4461b5c82ba6c6b1fd9c736dabf5d304cb7fbd60
Francis Ofori Anane <ofori.anane@gmail.com>
2023-10-18 16:37:36 +0000

    modified:   4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Executes task_wait_random n times."""
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)  # execute multiple coroutines
+
+    return sorted(delays)
